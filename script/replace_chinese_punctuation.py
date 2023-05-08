1adea18168cb0dd7a578c1447a4942e16812d47c
dirge <yujiawei@buaa.edu.cn>
2023-05-08 16:30:15 +0800

    add pseudocode examples

--- script/replace_chinese_punctuation.py
+++ script/replace_chinese_punctuation.py
@@ -4,6 +4,7 @@ def convert_punctuation_cn_to_en(text: str) -> str:
     cn_to_en_punctuation_map = {
         "。": ". ",
         "，": ", ",
+        "、": ", ",
         "“": "\"",
         "”": "\"",
         "‘": "'",
@@ -29,12 +30,20 @@ if __name__ == "__main__":
     if len(sys.argv)>1:
         # 命令行参数输入目标文件夹
         path = sys.argv[1]
+
+        with open(path, 'rb') as f:
+            text = f.read()
+            text = str(text, 'utf-8')
     else:
-        # 硬编码目标文件夹，请自行修改
-        path = "D:\yjw\course\Cryptography\CryptoLab\code\c8-ECC"
-    with open(path, 'rb') as f:
-        text = f.read()
-        text = str(text, 'utf-8')
+        path='C:\\Users\\y\'j\'w\\Desktop\\tmp1.md'
+        # 从stdin读取
+        text = ''
+        while True:
+            byte = sys.stdin.read(1)
+            if not byte:
+                break
+            else:
+                text += byte
 
     text = convert_punctuation_cn_to_en(text)
 
